59154ffe86c615d6aa00154498728ef6f33171f7
Tri Aji Bagaskara <90993802+Aji214@users.noreply.github.com>
2021-11-27 16:44:29 +0700

    Add files via upload

---
+++ 119140214_proses.php
@@ -0,0 +1,37 @@
+<?php
+include "119140214_koneksi.php";
+
+$nama = $_POST["nama"];
+$nim = $_POST["nim"];
+$instansi = $_POST["instansi"];
+$Jurusan = $_POST["Jurusan"];
+$email = $_POST['email'];
+$kontak = $_POST['kontak'];
+
+
+$foto = $_FILES['foto']['name'];
+$ekstensi = array('png', 'jpg', 'jpeg');
+$ukuran = $_FILES['foto']['size'];
+$ext = pathinfo($foto, PATHINFO_EXTENSION);
+
+if (!in_array($ext, $ekstensi)) {
+    header("location : 119140214_tugas_7.php?alert=ekstensi");
+}
+else {
+    if ($ukuran < 1500000) {
+        move_uploaded_file($_FILES['foto']['tmp_name'], 'foto/'.$foto);
+        $sql = "INSERT INTO webinar VALUES('$nim', '$nama', '$instansi', '$email', '$kontak', '$foto')";
+        $query = mysqli_query($kon, $sql);
+        header("location:119140214_tugas_7.php?alert=sukses");
+    }
+    else {
+        header("location:119140214_tugas_7.php?alert=ukuran");
+    }
+}
+
+
+
+if (!$query) {
+    die("Data Gagal Ditambahkan, Coba Lagi".mysqli_error($kon));
+}
+?>
